25366212164456c46d7dbf0bdff7c79975a30601
Nickson Kipruto Rop <nickyrutto@gmail.com>
2024-10-23 23:15:15 +0300

    add djangorest_simplejwt to list of installed apps

--- backend/vrias/vrias/settings.py
+++ backend/vrias/vrias/settings.py
@@ -37,9 +37,19 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "auth",
     "rest_framework",
+    "rest_framework_simplejwt",
+
+    "auth",
 ]
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ),
+    'DEFAULT_FILTER_BACKENDS':
+        ['django_filters.rest_framework.DjangoFilterBackend'],
+}
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
