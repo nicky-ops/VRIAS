5bd288d0e019d35392e78511303b5271da694c4c
Nickson Kipruto Rop <nickyrutto@gmail.com>
2024-10-23 23:13:37 +0300

    add avatar and bio fields

--- backend/vrias/auth/models.py
+++ backend/vrias/auth/models.py
@@ -68,6 +68,8 @@ class User(AbstractBaseUser, PermissionsMixin):
     is_staff = models.BooleanField(default=False)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
+    bio = models.TextField(blank=True)
+    avatar = models.ImageField(upload_to='avatars/', blank=True, null=True)
 
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = ['username']
