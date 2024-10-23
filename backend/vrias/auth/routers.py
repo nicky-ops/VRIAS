afaa2068619241c15bd5b63d5b7a55e1d164e1ce
Nickson Kipruto Rop <nickyrutto@gmail.com>
2024-10-23 23:13:11 +0300

    login and register serializers

--- backend/vrias/auth/routers.py
+++ backend/vrias/auth/routers.py
@@ -1,9 +1,11 @@
 from rest_framework import routers
-from .viewsets import UserViewSet
+from .viewsets import UserViewSet, RegisterViewSet, LoginViewSet
 
 router = routers.SimpleRouter()
 
-router.register(r'users', UserViewSet, basename='user')
+router.register(r'user', UserViewSet, basename='user')
+router.register(r'auth/register', RegisterViewSet, basename='auth-register')
+router.register(r'auth/login', LoginViewSet, basename='auth-login')
 
 urlpatterns = [
     *router.urls,
