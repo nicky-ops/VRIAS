0bfca715e97131dd3ff05bc47917cbf1b6d47648
Nickson Kipruto Rop <nickyrutto@gmail.com>
2024-10-25 00:28:20 +0300

    Token refresh viewset

--- backend/vrias/auth/viewsets.py
+++ backend/vrias/auth/viewsets.py
@@ -4,6 +4,7 @@ from rest_framework.response import Response
 from rest_framework import status
 from rest_framework_simplejwt.tokens import RefreshToken
 from rest_framework_simplejwt.exceptions import TokenError, InvalidToken
+from rest_framework_simplejwt.views import TokenRefreshView
 from .serializers import UserSerializer, RegisterSerializer, LoginSerializer
 from .models import User
 
@@ -66,6 +67,28 @@ class LoginViewSet(viewsets.ViewSet):
         Login a user
         '''
         serializer = self.serializer_class(data=request.data)
+
+        try:
+            serializer.is_valid(raise_exception=True)
+        except TokenError as e:
+            raise InvalidToken(e.args[0])
+        
+        return Response(serializer.validated_data, status=status.HTTP_200_OK)
+    
+
+
+class RefreshViewSet(viewsets.ViewSet, TokenRefreshView):
+    '''
+    Refresh token viewset
+    '''
+    permission_classes = (AllowAny,)
+    http_method_names = ['post']
+
+    def create(self, request, *args, **kwargs):
+        '''
+        Refresh a token)
+        '''
+        serializer = self.get_serializer(data=request.data)
 
         try:
             serializer.is_valid(raise_exception=True)
